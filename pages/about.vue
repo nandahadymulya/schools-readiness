04bfb6ae9ceac7013625ee8d2bae49d7ee29984e
Nanda Hady Mulya <nandahadymulyaa@gmail.com>
2022-05-14 21:06:02 +0700

    feat: slicing about page

--- pages/about.vue
+++ pages/about.vue
@@ -19,37 +19,97 @@
     <!-- End -->
 
     <!-- Section Blended-Learning -->
-    <section>
-      <div class="flex w-full">
-        <div class="w-1/2 text-center align-center">Text</div>
-        <div class="w-1/2">Gambar</div>
+    <section class="lg:py-28">
+      <div class="flex h-96 justify-between align-center">
+        <div
+          class="flex flex-col w-1/2 justify-center align-center text-center"
+        >
+          <p class="text-blue-500">Topic of Readiness</p>
+          <h1 class="text-4xl text-blue-900">Blended-Learning</h1>
+          <p class="font-light lg:pt-5">
+            Blended Learning is a learning strategy that can develop critical
+            thinking skills <br />
+            and optimize the use of information and technology <br />
+            as well as improve learning outcomes.
+          </p>
+        </div>
+
+        <div class="flex w-1/2 justify-center align-center">
+          <img
+            src="blendedlearning.jpg"
+            class="rounded-md"
+            alt="SCH Main Image"
+          />
+        </div>
       </div>
     </section>
     <!-- End -->
 
     <!-- Section K-Means -->
-    <section>
-      <div class="flex w-full">
-        <div class="w-1/2 text-center align-center">Text</div>
-        <div class="w-1/2">Gambar</div>
+    <section class="lg:py-28">
+      <div class="flex w-full h-96 justify-center align-center">
+        <div class="flex w-1/2 justify-center align-center">
+          <img src="kmeans.jpg" class="rounded-md" alt="SCH Main Image" />
+        </div>
+
+        <div
+          class="flex flex-col w-1/2 justify-center align-center text-center"
+        >
+          <p class="text-blue-500">Algorithm Technology</p>
+          <h1 class="text-4xl text-blue-900">K-Means</h1>
+          <p class="font-light lg:pt-5">
+            The K-Means algorithm is one of the Clustering algorithms <br />
+            which is included in the unsupervised learning group <br />
+            which is used to group data into several groups with a partition
+            system.
+          </p>
+        </div>
       </div>
     </section>
     <!-- End -->
 
     <!-- Section Support Vector Machines -->
-    <section>
-      <div class="flex w-full">
-        <div class="w-1/2 text-center align-center">Text</div>
-        <div class="w-1/2">Gambar</div>
+    <section class="lg:py-28">
+      <div class="flex w-full h-96 justify-center align-center">
+        <div
+          class="flex flex-col w-1/2 justify-center align-center text-center"
+        >
+          <p class="text-blue-500">Algorithm Technology</p>
+          <h1 class="text-4xl text-blue-900">Support Vector Machines</h1>
+          <p class="font-light lg:pt-5">
+            SVM or Support Vector Machine is a classification algorithm <br />
+            with the aim of finding a separator function that can separate two
+            data sets <br />
+            from two different classes.
+          </p>
+        </div>
+
+        <div class="flex w-1/2 justify-center align-center">
+          <img src="svm.jpg" class="rounded-md" alt="SCH Main Image" />
+        </div>
       </div>
     </section>
     <!-- End -->
 
     <!-- Section Developer -->
-    <section>
-      <div class="flex w-full">
-        <div class="w-1/2 text-center align-center">Text</div>
-        <div class="w-1/2">Gambar</div>
+    <section class="lg:py-28">
+      <div class="flex w-full h-96 justify-center align-center">
+        <div class="flex w-1/2 justify-center align-center">
+          <img src="developer.jpg" class="rounded-md" alt="SCH Main Image" />
+        </div>
+
+        <div
+          class="flex flex-col w-1/2 justify-center align-center text-center"
+        >
+          <p class="text-blue-500">Developer</p>
+          <h1 class="text-4xl text-blue-900">Nanda Hady Mulya</h1>
+          <p class="font-light lg:pt-5">
+            A final-year informatics engineering student, <br />
+            concentrated in Artificial Intelligence (AI) whose highly motivated,
+            visionary, <br />
+            and have the capability to be a groundbreaker.
+          </p>
+        </div>
       </div>
     </section>
     <!-- End -->
