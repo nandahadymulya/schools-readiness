61f0b6fb02fdda9a8f24189865a919fbed0859b0
Nanda Hady Mulya <nandahadymulyaa@gmail.com>
2022-05-18 19:32:36 +0700

    feat: add schools store

---
+++ pages/schools/_npsn.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    {{ getDetail[0] }}
+
+    <NuxtLink to="/schools">Back</NuxtLink>
+  </div>
+</template>
+<script>
+export default {
+  computed: {
+    npsn() {
+      return this.$route.params.npsn
+    },
+    getDetail() {
+      return this.$store.state.schools.schoolDetail
+    },
+  },
+  mounted() {
+    this.$store.dispatch('schools/fetchSchoolByNpsn', this.npsn)
+  },
+}
+</script>
