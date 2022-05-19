68c8cf40dd617643d8070de5af3ffc82065ca9f1
Nanda Hady Mulya <nandahadymulyaa@gmail.com>
2022-05-19 18:31:16 +0700

    feat: slicing detail school page

--- pages/schools/_npsn.vue
+++ pages/schools/_npsn.vue
@@ -1,8 +1,34 @@
 <template>
   <div>
-    {{ getDetail[0] }}
+    <div class="lg:pb-5">
+      <NuxtLink to="/schools" class="text-blue-500">Back</NuxtLink>
+    </div>
+    <!-- Main Cover -->
+    <main>
+      <div
+        class="flex w-full h-96 justify-center align-center text-center border rounded bg-gradient-to-r from-blue-900 to-blue-500"
+      >
+        <div class="flex flex-col justify-center">
+          <img src="assets/img/home.jpg" alt="Cover Detail Page" />
+        </div>
+      </div>
+    </main>
+    <!-- End -->
 
-    <NuxtLink to="/schools">Back</NuxtLink>
+    <!-- Section -->
+    <div class="flex py-28">
+      <img src="" alt="" />
+      <ul v-for="(detail, index) in getDetail" :key="(detail.sch_npsn, index)">
+        <li>School Name: {{ detail.sch_name }}</li>
+        <li>NPSN: {{ detail.sch_npsn }}</li>
+        <li>Address: {{ detail.sch_address }}</li>
+        <li>Regional: {{ detail.sch_kec }}</li>
+        <li>City: {{ detail.sch_kab_kota }}</li>
+        <li>Province: {{ detail.sch_province }}</li>
+        <li>ZIP Code: {{ detail.sch_zip_kode }}</li>
+      </ul>
+    </div>
+    <!-- End -->
   </div>
 </template>
 <script>
